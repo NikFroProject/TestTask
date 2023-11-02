9e4d5d5a4283ae0d160185a0160038af96a3a25e
Nikolay Frolov <frolovats2020@gmail.com>
2023-11-02 21:17:10 +0300

    Create tab_bar in MainActivity.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,18 @@ plugins {
 
 android {
     namespace = "com.nikolayfrolov.testtask"
-    compileSdk = 33
+    compileSdk = 32
+
+    //  Подключение binding
+    buildFeatures   {
+        viewBinding = true
+    }
+    //
 
     defaultConfig {
         applicationId = "com.nikolayfrolov.testtask"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
@@ -30,10 +36,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Совместимость с фрагментами
+    implementation("androidx.navigation:navigation-fragment:2.3.5")
+    implementation("androidx.navigation:navigation-ui:2.3.5")
+
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 }
--- app/src/main/java/com/nikolayfrolov/testtask/MainActivity.java
+++ app/src/main/java/com/nikolayfrolov/testtask/MainActivity.java
@@ -1,14 +1,78 @@
 package com.nikolayfrolov.testtask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.nikolayfrolov.testtask.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    MenuFragment menuFragment = new MenuFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    BasketFragment basketFragment = new BasketFragment();
+
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // FragmentManager для перехода между фрагментами
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment, new MenuFragment());
+        fragmentTransaction.commit();
+        //
+
+        bottomNavigationView = findViewById(R.id.tab_bar); // Инициализация bottomNavigationView
+
+        bottomNavigationView.setSelectedItemId(R.id.action_menu); // При запуске активна 1 кнопка нижней панели навигации
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, menuFragment).commit(); // Задан фрагмент, видимый по умолчанию, при запуске приложения
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() { // Установка нужного фрагмента по нажанию кнопки панели навигации
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if (R.id.action_menu == item.getItemId()) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, menuFragment).commit();
+                        return true;
+                    } else if (R.id.action_profile == item.getItemId()) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, profileFragment).commit();
+                        return true;
+                    } else if (R.id.action_basket == item.getItemId()) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, basketFragment).commit();
+                        return true;
+                    }
+
+//                switch (item.getItemId())   {
+//                    case R.id.action_menu:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, menuFragment).commit();
+//                        return true;
+//                    case R.id.action_profile:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, profileFragment).commit();
+//                        return true;
+//                    case R.id.action_basket:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, basketFragment).commit();
+//                        return true;
+//                }
+                return false;
+            }
+        });
+
     }
 }
